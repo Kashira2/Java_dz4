4340691828648ddc13d329d3a6ab80e1f93cd160
Kashira2 <nekit.egorov@icloud.com>
2023-03-16 00:12:44 +0300

    dz1

--- dz1.java
+++ dz1.java
@@ -1,6 +1,27 @@
+import java.util.LinkedList;
+import java.util.Random;
+
 public class dz1 {
 
     public static void main(String[] args) {
-        
+        LinkedList<Integer> link = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            link.add(i);
+        }
+
+        System.err.println(link.toString());
+
+        String res = reverseLink(link).toString();
+        System.out.println(res);
+    }
+
+    private static LinkedList reverseLink(LinkedList<Integer> link) {
+        LinkedList<Integer> link2 = new LinkedList<>();
+
+        for (int i = 0; i < link.size(); i++) {
+            link2.addFirst(link.get(i));
+        }
+
+        return link2;
     }
 }
