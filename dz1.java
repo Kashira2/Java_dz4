8f39deff4a441900e7d93ac48582677ce225d514
Kashira2 <nekit.egorov@icloud.com>
2023-03-15 22:58:29 +0300

    first commit

---
+++ dz1.java
@@ -0,0 +1,6 @@
+public class dz1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ dz2.java
@@ -0,0 +1,5 @@
+public class dz2 {
+    public static void main(String[] args) {
+        
+    }
+}
